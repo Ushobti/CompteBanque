b9d4260983bad7f18af8ff50795b28c40cf2e87f
cdaun <cdaun@10.151.129.93>
2018-02-02 11:48:35 +0100

    Ajout des fonctions déposer, retirer et virer.

--- src/Banque/Compte.java
+++ src/Banque/Compte.java
@@ -8,5 +8,20 @@ public class Compte {
 		// TODO Auto-generated constructor stub
 		Solde = (float) 0.0;
 	}
+	
+	public void deposer(float somme) {
+		Solde += somme;
+	}
+	
+	public float retirer(float somme) {
+		Solde -= somme;
+		return Solde ;
+	}
+	
+	public float virer(float somme, Compte compteDestination) {
+		this.retirer(somme);
+		compteDestination.deposer(somme);
+		return somme;
+	}
 
 }
